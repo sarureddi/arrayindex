512e8b928d5b7038b3d036dc19ee02dc43add4c3
saru <sarumathiayyappan@gmail.com>
2019-06-07 12:02:43 +0530

    Update arrayindex.py

--- arrayindex.py
+++ arrayindex.py
@@ -3,14 +3,14 @@ f1=list(map(int,input().split()))
 li1=[]
 l1=[]
 h1=[]
-for i1 in range(0,k1):
+for i11 in range(0,k1):
     u1,v1=map(int,input().split())
     for i in range(u1,v1+1):
-        li1.append(f1[i1-1])
+        li1.append(f1[i11-1])
     l1.append(sum(li1))
 h1.append(l1[0])
-for i1 in range(0,len(l1)-1):
-    s1=l1[i1+1]-l1[i1]
+for i11 in range(0,len(l1)-1):
+    s1=l1[i11+1]-l1[i11]
     h1.append(s1)
-for i1 in h1:
-    print(i1)
+for i11 in h1:
+    print(i11)
